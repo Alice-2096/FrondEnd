e56d5c887b44d5b286c97726b2aa874a55371439
Alice Jiang <xinranjiang2015@yahoo.com>
2022-08-03 11:33:30 -0400

    need to fix a few bugs

--- proj/grocery/app.js
+++ proj/grocery/app.js
@@ -17,6 +17,9 @@ let editID = "";
 form.addEventListener('submit', addItem); 
 //clear items 
 clearBtn.addEventListener('click', clearItems); 
+//load items 
+window.addEventListener('DOMContentLoaded', setupItems); 
+
 
 // ****** FUNCTIONS **********
 function addItem(e){
@@ -24,31 +27,7 @@ function addItem(e){
     const value = grocery.value; 
     const id = new Date.getTime().toString(); 
     if (value && !editFlag){
-        const element = document.createElement('article'); 
-        // add class 
-        element.classList.add('grocery'); 
-        // add id 
-        const attr = document.createAttribute('data-id'); 
-        attr.value = id; 
-        element.setAttributeNode(attr); 
-        element.innerHTML = `<p class="title"> ${value}</p>
-                <div class="btn-container">
-                <!-- edit button -->
-                <button type="button" class="edit-btn">
-                    <i class="fas fa-edit"></i>
-                </button>
-                <!-- delete button -->
-                <button type="button" class="delete-btn">
-                    <i class="fas fa-trash"></i>
-                </button>
-                </div>`
-        // give us access to the dynamic buttons 
-        const deleteBtn = element.querySelector('.delete-btn'); 
-        const editBtn = element.querySelector('.edit-btn'); 
-        deleteBtn.addEventListener('click', deleteItem);
-        editBtn.addEventListener('click', editItem); 
-        // add child 
-        list.appendChild(element); 
+        createListItem(id, value); 
         //display alert 
         displayAlert('item added to the list', 'success'); 
         // show container 
@@ -133,11 +112,85 @@ function clearItems() {
 }
 
 // ****** LOCAL STORAGE **********
+
+
+/* Local storage API: 
+    setItem, getItem, removeItem, save as strings using JSON.stringify, parse to get the value back
+
+localStorage.addItem("orange", JSON.stringify(['A', 'B', 'C']));
+const oranges = JSON.parse(localStorage.getItem("oranges")); 
+localStorage.removeItem("orange");  
+*/
+
+
 function addToLocalStorage(id, value){
-    // console.log("added to local storage"); 
+    const grocery = {id: id, value: value}; 
+    let items = getLocalStorage();
+    items.push(grocery); 
+    localStorage.setItem('list', JSON.stringify(items)); 
 }
-function removeFromLocalStorage(id){}
-function editLocalStorage(id, value){}
-// Local storage API: setItem, getItem, removeItem, save as strings using JSON.stringify 
-localStorage.setItem()
+function removeFromLocalStorage(id){
+    let items = getLocalStorage(); 
+    items = items.filter(function(item){ //filter out items that does not satisfy the function 
+        if(item.id !== id){
+            return item; 
+        }
+    }); 
+    localStorage.setItem("list", JSON.stringify(items)); 
+}
+function editLocalStorage(id, value){
+    items = getLocalStorage(); 
+    // get item from the list by id and set new value 
+    items = items.map(function(item){
+        if (item.id === id){
+            item.value = value; 
+        }
+        return item; 
+    })
+}
+
+function getLocalStorage(){
+    // if null, then return an empty array; if not, return array  
+    return localStorage.getItem("list")?JSON.parse(localStorage.getItem("list")): [];  
+}
+
 // ****** SETUP ITEMS **********
+function setupItems(){
+    let items = getLocalStorage(); 
+    if (items.length > 0){
+        items.forEach(function(item){
+            createListItem(item.id, item.value); 
+            
+        })
+    }
+}
+
+function createListItem(id, value){
+    const element = document.createElement('article'); 
+        // add class 
+        element.classList.add('grocery-item'); 
+        // add id 
+        const attr = document.createAttribute('data-id'); 
+        attr.value = id; 
+        element.setAttributeNode(attr); 
+        element.innerHTML = 
+                `<p class="title"> ${value}</p>
+                <div class="btn-container">
+                <!-- edit button -->
+                <button type="button" class="edit-btn">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <!-- delete button -->
+                <button type="button" class="delete-btn">
+                    <i class="fas fa-trash"></i>
+                </button>
+                </div>`
+        // give us access to the dynamic buttons 
+        const deleteBtn = element.querySelector('.delete-btn'); 
+        const editBtn = element.querySelector('.edit-btn'); 
+        deleteBtn.addEventListener('click', deleteItem);
+        editBtn.addEventListener('click', editItem); 
+        // add child 
+        list.appendChild(element); 
+}
+
